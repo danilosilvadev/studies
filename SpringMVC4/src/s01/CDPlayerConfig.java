063b99fcf0ba85047558bfabc9c326e71fdcb25b
GorickUPM <danilosilvaweb@hotmail.com>
2017-05-10 16:45:24 -0300

    First test of DI

---
+++ SpringMVC4/src/s01/CDPlayerConfig.java
@@ -0,0 +1,13 @@
+package s01;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by sg-0036936 on 10/05/2017.
+ */
+
+@Configuration
+@ComponentScan
+public class CDPlayerConfig {
+}
---
+++ SpringMVC4/src/s01/CompactDisc.java
@@ -0,0 +1,13 @@
+package s01;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by sg-0036936 on 10/05/2017.
+ */
+@Component
+public class CompactDisc {
+
+    private String whatever;
+
+}
